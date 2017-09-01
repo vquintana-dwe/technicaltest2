5bf6e51e86d33bfd462d8f88f6a28c5fd692197d
victor <victor.quintana@dwellant.com>
2017-09-01 16:23:16 +0100

    Initial version

---
+++ LogicTest/LogicTest/RequestForQuotation.cs
@@ -0,0 +1,19 @@
+﻿namespace LogicTest
+{
+    public class RequestForQuotation
+    {
+        /*
+            Finish the GetBetterPrice function so it return string just like in the examples below:
+
+            GetBetterPrice(["The Yellow Elephant", "The Happy Cake"],[1000.00m, 1000.01m]) // "Between The Yellow Elephant and The Happy Cake, The Yellow Elephant has a better price!"
+            GetBetterPrice(["Silly Tomato", "The Red Shark"],[400.00m, 200.00m]) // "Between Silly Tomato and The The Red Shark, The Red Shark has a better price!"
+            GetBetterPrice(["The Small Panda", "The Grey Spoon"],[150.00m, 150.00m]) // "There is no difference of price between The Small Panda and The Grey Spoon."
+         */
+
+        public static string GetBetterPrice(string[] contractors, decimal[] quotations)
+        {
+
+            return null;
+        }
+    }
+}
---
+++ LogicTest/LogicTest/Tests/RequestForQuotationTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicTest.Tests
+{
+    
+    [TestClass]
+    public class RequestForQuotationTests
+    {
+        [TestMethod]
+        public void GetBetterPriceTests()
+        {
+            Assert.AreEqual("Between The Yellow Elephant and The Happy Cake, The Yellow Elephant has a better price!",
+                RequestForQuotation.GetBetterPrice(new[] {"The Yellow Elephant", "The Happy Cake"},
+                    new[] {1000.00m, 1000.01m}));
+
+            Assert.AreEqual("Between Silly Tomato and The The Red Shark, The Red Shark has a better price!",
+                RequestForQuotation.GetBetterPrice(new[] {"Silly Tomato", "The Red Shark"}, new[] {400.00m, 200.00m}));
+
+            Assert.AreEqual("There is no difference of price between The Small Panda and The Grey Spoon.",
+                RequestForQuotation.GetBetterPrice(new[] {"The Small Panda", "The Grey Spoon"},
+                    new[] {150.00m, 150.00m}));
+        }
+    }
+}
